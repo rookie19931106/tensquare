cfd65121ec82c9df25ede38f1c41561df849cede
jay <1021038907@qq.com>
2018-11-27 22:48:58 +0800

    firstCommit

---
+++ tensquare-common/src/main/java/entity/StatusCode.java
@@ -0,0 +1,23 @@
+package entity;
+
+/**
+ * @author jay
+ * @date 2018/11/27 0:04
+ * 代码状态
+ */
+public class StatusCode {
+
+    //成功
+    public static final int OK = 20000;
+    //失败
+    public static final int ERROR = 20001;
+    //登录失败
+    public static final int LOGINERROR = 20002;
+    //权限不足
+    public static final int ACCESSERROR = 20003;
+    //远程连接失败
+    public static final int REMOTEERROR = 20004;
+    //重复
+    public static final int REPERROR = 20005;
+
+}
